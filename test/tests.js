6752034f86a63e84af328eafa7581e70506044c9
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-05-06 14:26:03 -0700

    created test for API and separated all test files

--- test/tests.js
+++ test/tests.js
@@ -1,24 +1,2 @@
-import makePortlander from '../src/home/make-portlander.js';
-const test = QUnit.test;
-
-test('time to test a function', function(assert) {
-    //Arrange
-    // Set up your parameters and expectations
-    const formData = new FormData();
-    formData.set('name', 'claire');
-    formData.set('portlander', 'hiker');
-
-    const expected = {
-        name: 'claire',
-        portlander: 'hiker',
-        wellBeing: '100',
-        localCred: '0'
-    };
-
-    // Act
-    // Call the function you're testing and set the result to a const
-    const portlander = makePortlander(formData);
-
-    //Assert
-    assert.deepEqual(portlander, expected);
-});
+import './api.test.js';
+import './form-data.test.js';
