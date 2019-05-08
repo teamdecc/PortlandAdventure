90af70be46274249167135af00a19afd8c2e18e1
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-05-08 14:54:00 -0700

    tested and created score-choice

--- test/score-choice.test.js
+++ test/score-choice.test.js
@@ -1,10 +1,38 @@
+import api from '../src/services/api.js';
+import scoreChoice from '../src/quests/score-choice.js';
+
 const test = QUnit.test;
 
-test('score the user choice in the profile', assert => {
+test('update WB and LC based on user choice and save to api', assert => {
     //arrange
-    
-    //act
+    const user = {
+        name: 'claire',
+        portlander: 'hiker',
+        wellBeing: 100,
+        localCred: 0,
+        completed: {}
+    };
 
+    const choice = {
+        LC: 5, 
+        WB: -90
+    };
+
+    const quest = {
+        id: 'cheeseburger'
+    };
+
+    const expected = {
+        name: 'claire',
+        portlander: 'hiker',
+        wellBeing: 10,
+        localCred: 5,
+        completed: {
+            cheeseburger: true
+        }
+    };
+    //act
+    const result = scoreChoice(user, choice, quest);
     //assert
-    assert.equal(result, expected);
+    assert.deepEqual(result, expected);
 });
