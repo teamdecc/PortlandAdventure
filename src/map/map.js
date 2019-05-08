0d9e7f8f18f76875c1c8d93b0fb0f9a94e088e2d
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-05-07 17:50:47 -0700

    imported quest data

--- src/map/map.js
+++ src/map/map.js
@@ -1,18 +1,5 @@
 import api from '../services/api.js';
 import createCompletedQuest from '../map/create-completed-quest.js';
 import createQuestLink from '../map/create-quest-link.js';
-
-const avatar = document.getElementById('avatar');
-const name = document.getElementById('name');
-const wellBeing = document.getElementById('well-being');
-const localCred = document.getElementById('local-cred');
-
-const user = api.getPortlander();
-console.log('user');
-
-name.textContent = user.name;
-avatar.src = '/assets/' + user.portlander + '.svg';
-wellBeing.textContent = 'Well Being: ' + user.wellBeing;
-localCred.textContent = 'Local Cred: ' + user.localCred;
-
-console.log(user.portlander);
+import makeProfile from '../make-profile.js';
+makeProfile();
