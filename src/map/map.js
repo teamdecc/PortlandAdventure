fa595998a12d4bc0d1720e64e714caf430f313d7
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-05-09 14:14:42 -0700

    Imported createCompletedQuest

--- src/map/map.js
+++ src/map/map.js
@@ -1,16 +1,23 @@
 import api from '../services/api.js';
 import createQuestLink from '../map/create-quest-link.js';
 import makeProfile from '../make-profile.js';
+import createCompletedQuest from './create-completed-quest.js';
+
 makeProfile();
 
 const questList = document.getElementById('quest-list');
 
 const quests = api.getQuests();
+const user = api.getPortlander();
 
 for(let i = 0; i < quests.length; i++){
     const quest = quests[i];
-    const link = createQuestLink(quest);
+    let link = null;
 
+    if(user.completed[quest.id]) {
+        link = createCompletedQuest(quest);
+    } else {
+        link = createQuestLink(quest);
+    }
     questList.appendChild(link);
-    
 }
