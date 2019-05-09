03c5174fae0f8bf00284c0b0a02b1bf49e9b4777
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-05-09 15:18:30 -0700

    Created dictionaries for each end game result message

---
+++ src/end/wellbeing-message.js
@@ -0,0 +1,7 @@
+const wellBeingMessages = {
+    dead: 'Your wellbeing really took a bad turn. You pass into the next life.',
+    sick: 'You have taken a beating, but you are still kicking.',
+    healthy: 'Your portland adventure has left you refreshed and healthy.'
+};
+
+export default wellBeingMessages;
