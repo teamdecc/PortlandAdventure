e50b3e530e19f9f3cbd0a41c3eaa9fada312c380
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-05-09 14:22:41 -0700

    check status completed quests and wellbeing on map page

--- src/map/map.js
+++ src/map/map.js
@@ -1,14 +1,18 @@
 import api from '../services/api.js';
-import createQuestLink from '../map/create-quest-link.js';
 import makeProfile from '../make-profile.js';
+import createQuestLink from '../map/create-quest-link.js';
 import createCompletedQuest from './create-completed-quest.js';
+import hasCompletedAllQuests from '../map/has-completed-all-quests.js';
+import isDead from '../map/is-dead.js';
 
 makeProfile();
-
-const questList = document.getElementById('quest-list');
-
 const quests = api.getQuests();
 const user = api.getPortlander();
+
+if(isDead(user) || hasCompletedAllQuests(quests, user)) {
+    window.location = 'end.html';
+}
+const questList = document.getElementById('quest-list');
 
 for(let i = 0; i < quests.length; i++){
     const quest = quests[i];
